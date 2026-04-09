3a185f75a9321c8d0907848360eee58bfc0c16d2
inakondu1 <inaisah66@gmail.com>
2026-04-09 01:19:59 +0100

    printing hex in an index form

--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
+
 func main() {
 	text := "1E (hex) files were added"
+	// in here i need to split hte word so i will be using strings.Fields. then i will range through the word. the if statement is me planning on replacing hex in the future run
 	word := strings.Fields(text)
 	for i, w := range word {
-// this i meeans index, w is word. i want to print them out in an index formatt
+		if w == "(hex)" {
+			previousword := word[i-1]
+			// the lines of code below is where the conversion begine. this lines is expected to bring an out put of 30 after runing the code.
+			decimal, err := strconv.ParseInt(previousword, 16, 64)
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+			fmt.Println(decimal)
+		}
+		// this 'i' meeans index, w is word. i want to print them out in an index formatt
 		fmt.Println(i, w)
 	}
-	   fmt.Println(word)
+	fmt.Println(word)
 
 }
